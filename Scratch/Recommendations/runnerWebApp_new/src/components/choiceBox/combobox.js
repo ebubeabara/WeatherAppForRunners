19509c068c0df1b90df21db780ab15c2002d5bc8
Swapnil Holkar <Swapnil@Swapnils-MacBook-Pro.local>
2017-03-09 12:42:52 +0000

    new version of recommedations

---
+++ Scratch/Recommendations/runnerWebApp_new/src/components/choiceBox/combobox.js
@@ -0,0 +1,64 @@
+
+import { h, render, Component } from 'preact';
+import $ from 'jquery';
+
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+export default class gymDropdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+}
+
+
+Dropdown.propTypes = {
+  disabled: PropTypes.bool,
+  dropup: PropTypes.bool,
+  group: PropTypes.bool,
+  isOpen: PropTypes.bool,
+  tag: PropTypes.string, // default: 'div'
+  tether: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  toggle: PropTypes.func
+};
+
+DropdownToggle.propTypes = {
+  caret: PropTypes.bool,
+  color: PropTypes.string,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  'data-toggle': PropTypes.string,
+  'aria-haspopup': PropTypes.bool,
+  // For DropdownToggle usage inside a Nav
+  nav: PropTypes.bool,
+  // Defaults to Button component
+  tag: PropTypes.any
+};
